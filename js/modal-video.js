8c4a7dd0e53a2117b824e772ea3b7c6fc227b47e
Maria Martinez Bonilla <mmartinez@b7-23.local>
2017-08-11 08:56:44 -0600

    Entrepreneurship project web site

---
+++ js/modal-video.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+    // Functions
+    // Toggles the visibility of the modals
+    function toggleVisibility() {
+        if(!$('#videoModal').hasClass('hidden')) {
+            $('.opacity').addClass('hidden');
+            $('#videoModal').addClass('hidden');            
+        }
+    }
+    
+    // When you click the trailer button
+    $('#playVideo').click(function() {
+		$('#videoModal')[0].innerHTML = '<video  width="1000" height="600" controls><source src="video/Dog-Lovers.mp4" type="video/mp4">Your browser does not support HTML5 video.</video>';
+        $('.opacity').removeClass('hidden');
+        $('#videoModal').removeClass('hidden');
+    })
+
+    // When you click the arrow, hides the modal
+    $('.btn-close').click(function() {
+        toggleVisibility();
+		$('#videoModal')[0].innerHTML = '';
+    })
+
+    // When you click the opacity the modal hides
+    $('.opacity').click(function() {
+        toggleVisibility();
+		$('#videoModal')[0].innerHTML = '';
+    })
+});
